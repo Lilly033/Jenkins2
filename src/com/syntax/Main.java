4c84eba098bca862b0e8d5d13bc02ded76f1f3d7
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-06-22 09:39:05 -0700

    My first jenkins project

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello! i am a cat");
+
+        }
+    }
+}
