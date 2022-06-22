68794269a39ca1973bfc3fd934109ee57c76b2b6
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-06-22 10:15:28 -0700

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Hello! i am a cat");
+            System.out.println("Hello ! i am a dog");
 
         }
     }
